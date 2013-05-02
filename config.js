9c13f8bac6f145aa90fb23b39e9ef71435c714e6
Anthony Neal and Roy van de Water <sm+anthony+roy@pearson.com>
2013-05-02 14:02:54 -0700

    Add different sfx

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 // e.g. http://ci.example.com/api/json
 
 var config = {
-	ci_json_url:"http://ci.example.com/api/json",
-	refresh_interval: 60000,
-	filtered: ["Filtered Build"],
-	all_green_html: '<i class="icon-heart icon-white"></i>'
+  ci_json_url:"http://ci.smdev.info/view/Failed/api/json",
+  refresh_interval: 60000,
+  filtered: ["Filtered Build"],
+  all_green_html: '<i class="icon-heart icon-white"></i>'
 }
